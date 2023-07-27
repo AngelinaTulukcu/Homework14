69c6349b4217aec7d60841c149b3b99d850c123d
Angelina <a.tulukcu@teamprocure.com>
2023-07-27 18:27:46 +0300

    Task 18 - updated

--- src/test/java/org/example/DeliveryWeb.java
+++ src/test/java/org/example/DeliveryWeb.java
@@ -1,28 +1,35 @@
 package org.example;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-// Task 18
-public class DeliveryWeb {
 
-    // 1.1.
+public class DeliveryWeb{
+
+    @AfterEach
+    public void tearDown(){
+        closeWebDriver();
+    }
+
+    // 1.1
     @Test
     public void insertIncorrectLoginPasswordAndCheckError(){
 
         open("http://51.250.6.164:3000/signin");
 
-        $(By.xpath("//input[@data-name='username-input']")).setValue("12345");
-        $(By.xpath("//input[@data-name='password-input']")).setValue ("000");
+        $(By.xpath("//input[@data-name='username-input']")).setValue("1111");
+        $(By.xpath("//input[@data-name='password-input']")).setValue ("1111");
+
         $(By.xpath("//button[@data-name='signIn-button']")).click();
+
         $(By.xpath("//div[@data-name='authorizationError-popup']")).shouldBe(Condition.exist, Condition.visible);
     }
 
-    // 1.2.
+    // 1.2
     @Test
     public void insertCorrectLoginPasswordAndCheckError(){
 
@@ -46,8 +53,9 @@ public class DeliveryWeb {
         SetupFunctions setupFunctions = new SetupFunctions();
 
         open("http://51.250.6.164:3000/signin");
-        $(By.xpath("//input[@data-name='username-input']")).setValue("login");
-        $(By.xpath("//input[@data-name='password-input']")).setValue("password");
+        $(By.xpath("//input[@data-name='username-input']")).setValue("Angelina");
+        $(By.xpath("//input[@data-name='password-input']")).setValue("test");
+
         $(By.xpath("//button[@data-name='signIn-button']")).click();
 
         $(By.xpath("//button[@data-name='authorizationError-popup-close-button']")).shouldBe(Condition.exist, Condition.visible);
@@ -56,7 +64,9 @@ public class DeliveryWeb {
 
         $(By.xpath("//input[@data-name='username-input']")).setValue(setupFunctions.getUsername());
         $(By.xpath("//input[@data-name='password-input']")).setValue(setupFunctions.getPassword());
+
         $(By.xpath("//button[@data-name='signIn-button']")).click();
+
         $(By.xpath("//input[@data-name='phone-input']")).shouldBe(Condition.exist, Condition.visible);
 
     }
@@ -69,7 +79,9 @@ public class DeliveryWeb {
 
         $(By.xpath("//input[@data-name='username-input']")).setValue("1");
         $(By.xpath("//input[@data-name='password-input']")).setValue("password");
+
         $(By.xpath("//button[@data-name='signIn-button']")).shouldBe(Condition.disabled);
+        $(By.xpath("//*[@data-name='username-input']/..//span[@data-name='username-input-error']")).shouldBe(Condition.exist, Condition.visible);
 
     }
 
@@ -79,20 +91,24 @@ public class DeliveryWeb {
 
         open("http://51.250.6.164:3000/signin");
 
-        $(By.xpath("//input[@data-name='username-input']")).setValue("12");
-        $(By.xpath("//input[@data-name='password-input']")).setValue("1");
+        $(By.xpath("//input[@data-name='username-input']")).setValue("An");
+        $(By.xpath("//input[@data-name='password-input']")).setValue("K");
+
         $(By.xpath("//button[@data-name='signIn-button']")).shouldBe(Condition.disabled);
+        $(By.xpath("//*[@data-name='password-input']/..//span[@data-name='username-input-error']")).shouldBe(Condition.exist,Condition.visible);
 
     }
 
     // 1.4.2
     @Test
     public void buttonSignInInactiveLoginSpacePasswordSpace() {
+
         open("http://51.250.6.164:3000/signin");
 
         $(By.xpath("//input[@data-name='username-input']")).setValue("null");
         $(By.xpath("//input[@data-name='password-input']")).setValue("null");
-        $(By.xpath("//button[@data-name='signIn-button']")).shouldBe(Condition.disabled);
 
+        $(By.xpath("//button[@data-name='signIn-button']")).shouldBe(Condition.disabled);
+        $(By.xpath("//*[@data-name='password-input']/..//span[@data-name='username-input-error']")).shouldBe(Condition.exist,Condition.visible);
     }
 }
